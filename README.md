4d6c743686f4c5ef1f4163e562051c0206aff445
Huaghty <>
2025-01-10 09:08:01 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Vue 3 + Vite
+
+### A css3 animation effects project, continuously updated.
+### 一款css3动画效果项目，持续更新。
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="flex__box">
+  <component :is="currentComponent"></component>
+</div>
+</template>
+<script setup>
+import HelloWorld from "@/components/avatar/index.vue";
+import { reactive, defineAsyncComponent } from "vue";
+
+let currentComponent = defineAsyncComponent(async () =>
+  import(`@/components/avatar/index.vue`)
+)
+</script>
+
+<style scoped>
+.flex__box {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+}
+</style>
